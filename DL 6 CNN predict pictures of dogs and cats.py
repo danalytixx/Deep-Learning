c9114ffbe3e4d1b37ff89c88ede06765b189adaf
Danalytix <63015231+danalytixx@users.noreply.github.com>
2020-06-27 16:06:13 +0200

    Update and rename CNN dog or cat.py to DL 6 CNN predict pictures of dogs and cats.py

--- DL 6 CNN predict pictures of dogs and cats.py
+++ DL 6 CNN predict pictures of dogs and cats.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
@@ -106,7 +101,7 @@ plt.imshow(training_setnp[0])
 plt.show()
 
 
-# In[ ]:
+
 
 
 
